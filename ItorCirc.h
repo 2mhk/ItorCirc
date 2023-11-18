e883463bfd59c0929d25a0d8b533065d2e4b70b9
2mhk <907508272@qq.com>
2023-11-18 13:59:33 +0800

    Update ItorCirc.h

--- ItorCirc.h
+++ ItorCirc.h
@@ -5,7 +5,7 @@
 template<class T>
 class ItorCirc {
     using T_iterator = typename T::iterator;
-    using T_value_type = typename T::iterator::value_type;
+    using T_value_type = typename T::value_type;
 public:
     ItorCirc(T& data) :data_wrappered(data) { itor = data.begin(); }
     ItorCirc(T& data, T_iterator& itor_default) :data_wrappered(data), itor(itor_default) { }
